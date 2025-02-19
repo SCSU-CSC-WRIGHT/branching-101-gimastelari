d24a6b2d0b09941c06a66c242f3b49281abefd86
SanteIsHere <r.a.frye00@gmail.com>
2025-02-19 16:33:16 -0500

    Converted code in main.py to a function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
-for i in range(5):
-    number = input("Enter a number: ")
-    total += number
+def main():
+    total = 0
+    for i in range(5):
+        number = int(input("Enter a number: "))
+        total += number
 
-print("The running total is: "  total)
+    print("The running total is: ", total)
+
+main()
